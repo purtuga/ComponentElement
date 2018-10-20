cf615d54798bfe5b86fb726700f84194008d0dd8
Paul T <paultavares1@gmail.com>
2018-10-20 16:14:05 -0400

    Upgrade project-base package ++ new built files

--- src/decorators.js
+++ src/decorators.js
@@ -1,6 +1,6 @@
 export * from "./decorators/prop"
 export * from "./decorators/bind"
 
-console.warn("ComponentElement/src/decorators.js deprecated. Fix your code");
+console.warn("ComponentElement/src/decorators.js deprecated. Fix your code"); // eslint-disable-line
 
 // FIXME: delete this file
